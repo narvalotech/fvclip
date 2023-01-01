361cdd46909712260709a0e6891e06e559cdfb41
Jonathan Rico <jonathan@rico.live>
2023-01-01 23:44:50 +0100

    boot FV-1
    configure GPIO pins
    attempt to boot the FV-1 and loop through the internal programs.

--- src/main.c
+++ src/main.c
@@ -2,6 +2,8 @@
 #include <string.h>
 
 #include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
 #include <stdio.h>
 
 #include "eeprom.h"
@@ -17,6 +19,57 @@ void make_fake_data(uint8_t * data, size_t bytes)
 	for (int a=0; a<bytes; a++) {
 		data[a] = a & 0xFF;
 	}
+}
+
+#define GPIO_S0_PIN DT_PHA(DT_PATH(outputs, gpio_s0), gpios, pin)
+#define GPIO_S1_PIN DT_PHA(DT_PATH(outputs, gpio_s1), gpios, pin)
+#define GPIO_S2_PIN DT_PHA(DT_PATH(outputs, gpio_s2), gpios, pin)
+#define GPIO_EXT_PIN DT_PHA(DT_PATH(outputs, gpio_ext), gpios, pin)
+#define GPIO_CLIP_PIN DT_PHA(DT_PATH(inputs, gpio_clip), gpios, pin)
+
+void init_gpios(void)
+{
+	const struct device *port =
+		DEVICE_DT_GET(DT_PHANDLE(DT_PATH(outputs, gpio_s0), gpios));
+
+	uint32_t out_flags = GPIO_OUTPUT;
+	uint32_t in_flags = GPIO_INPUT;
+
+	gpio_pin_configure(port, GPIO_S0_PIN, out_flags);
+	gpio_pin_configure(port, GPIO_S1_PIN, out_flags);
+	gpio_pin_configure(port, GPIO_S2_PIN, out_flags);
+	gpio_pin_configure(port, GPIO_EXT_PIN, out_flags);
+
+	gpio_pin_configure(port, GPIO_CLIP_PIN, in_flags);
+}
+
+void select_program(uint8_t id)
+{
+	const struct device *port =
+		DEVICE_DT_GET(DT_PHANDLE(DT_PATH(outputs, gpio_s0), gpios));
+
+	if (id > 7) {
+		LOG_ERR("Program ID out of bounds (> 7)");
+		return;
+	}
+
+	LOG_INF("select program %u", id);
+	gpio_pin_set(port, GPIO_S0_PIN, id & 1);
+	gpio_pin_set(port, GPIO_S1_PIN, id & 2);
+	gpio_pin_set(port, GPIO_S2_PIN, id & 4);
+}
+
+enum {
+	PGM_BUILTIN = 0,
+	PGM_EXTERNAL,
+};
+
+void select_program_source(bool ext)
+{
+	const struct device *port =
+		DEVICE_DT_GET(DT_PHANDLE(DT_PATH(outputs, gpio_s0), gpios));
+
+	gpio_pin_set(port, GPIO_EXT_PIN, ext);
 }
 
 void main(void)
