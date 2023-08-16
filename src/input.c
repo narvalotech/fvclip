cc421dc2625278b0147893322c47b073b42ce3d2
Jonathan Rico <jonathan@rico.live>
2023-08-16 22:52:32 +0200

    try logging qdec events

---
+++ src/input.c
@@ -0,0 +1,14 @@
+#include <zephyr/kernel.h>
+#include <zephyr/input/input.h>
+#include <zephyr/device.h>
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(input_app, 4);
+
+static void input_cb(struct input_event *evt)
+{
+	LOG_ERR("%s: sync %u type %u code 0x%x value %d", __func__,
+		evt->sync, evt->type, evt->code, evt->value);
+}
+
+INPUT_LISTENER_CB_DEFINE(DEVICE_DT_GET(DT_PATH(encoder)), input_cb);
